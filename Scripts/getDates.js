781132a170a5a56cbbd5a65bdb332849ce872ff1
Fiijoey <kofimensah3730@gmail.com>
2025-01-18 00:06:55 +0000

    fg

--- Scripts/getDates.js
+++ Scripts/getDates.js
@@ -11,9 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
-
-
+document.getElementById("currentyear").innerHTML = year;
+document.getElementById("lastModified").innerHTML = modifiedt;
 
 const date = new Date();
 const form = new Intl.DateTimeFormat("en-us", {
@@ -22,6 +21,3 @@ const form = new Intl.DateTimeFormat("en-us", {
 });
 var year = date.getFullYear();
 var modifiedt = form.format(new Date(document.lastModified));
-
-document.getElementById("currentyear").innerHTML = year;
-document.getElementById("lastModified").innerHTML = modifiedt;
