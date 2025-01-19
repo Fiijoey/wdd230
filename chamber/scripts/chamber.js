d7ba5fd163e4a86ec8a16ce9b0be70f27ede53d4
Fiijoey <kofimensah3730@gmail.com>
2025-01-19 16:22:56 +0000

    home done

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -5,3 +5,13 @@ hamButton.addEventListener("click", () => {
   navigation.classList.toggle("open");
   hamButton.classList.toggle("open");
 });
+
+const date = new Date();
+const form = new Intl.DateTimeFormat("en-us", {
+  dateStyle: "full",
+  timeStyle: "short",
+});
+var year = date.getFullYear();
+var modifiedt = form.format(new Date(document.lastModified));
+
+document.getElementById("currentyear").innerHTML = year;
