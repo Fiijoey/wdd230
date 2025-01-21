6781f42578731ea998a4288e9e4addf40092cd78
Fiijoey <kofimensah3730@gmail.com>
2025-01-21 17:23:20 +0000

    lazy loading

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,10 @@
+const date = new Date();
+const form = new Intl.DateTimeFormat("en-us", {
+  dateStyle: "full",
+  timeStyle: "short",
+});
+var year = date.getFullYear();
+var modifiedt = form.format(new Date(document.lastModified));
+
+document.getElementById("currentyear").innerHTML = year;
+document.getElementById("lastModified").innerHTML = modifiedt;
