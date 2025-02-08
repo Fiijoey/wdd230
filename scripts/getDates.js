378676724ce48cbdf44d05fae16b52907d3a6c48
Fiijoey <kofimensah3730@gmail.com>
2025-02-08 17:43:04 +0000

    feat: integrate weather API for dynamic temperature and icon display, update visit count tracking

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -46,3 +46,50 @@ var modifiedt = form.format(new Date(document.lastModified));
 
 document.getElementById("currentyear").innerHTML = year;
 document.getElementById("lastModified").innerHTML = modifiedt;
+
+const currentTemp = document.querySelector("#current-temp");
+const currentIcon = document.querySelector("#weather-icon");
+const currentDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=5.556&lon=0.1969&units=imperal&appid=9cafa123f783487bdb6face6f1d55796";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  currentIcon.setAttribute("loading", "lazy");
+  currentIcon.setAttribute("alt", "weather icon");
+  currentIcon.setAttribute("width", "50");
+  currentIcon.setAttribute("height", "50");
+  currentIcon.setAttribute("src", `${iconsrc}`);
+  currentDesc.textContent = `${desc}`;
+}
+
+if (localStorage.getItem("visitCount")) {
+  localStorage.setItem(
+    "visitCount",
+    Number(localStorage.getItem("visitCount")) + 1
+  );
+} else {
+  localStorage.setItem("visitCount", 1);
+}
+document.querySelector(".numberofVisit").textContent =
+  localStorage.getItem("visitCount");
